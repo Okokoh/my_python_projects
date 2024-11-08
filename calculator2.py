bb43c92e9128a720165f8d6044b5c1652045356d
Raymond Okokoh <Okokoh_R@ukw.de>
2024-11-08 14:54:23 +0100

    A more explanatory calculator

---
+++ calculator2.py
@@ -0,0 +1,8 @@
+# this program does calculations for the user
+
+# ask user for the values of x and y
+
+x = int(input("what is x?: "))
+y = int(input("what is y?: "))
+
+print("sum of x and y is", x + y)
